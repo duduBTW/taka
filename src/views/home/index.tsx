2743fce36c6a32e40925339f786391cd96583287
Carlos Alves <carloseduardo108090@hotmail.com>
2021-01-17 17:26:35 -0300

    twitch

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,7 +1,30 @@
 import React from "react";
 
 import Header from "../../components/commission/Header";
+import {
+  TwitchEmbed,
+  TwitchChat,
+  TwitchClip,
+  TwitchPlayer,
+  //@ts-ignore
+} from "react-twitch-embed";
 
 export default function Home() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <h1>Twitch</h1>
+      <br />
+      <br />
+      <TwitchEmbed
+        channel="taaakaaaa"
+        id="taaakaaaa"
+        theme="dark"
+        muted
+        onVideoPause={() => console.log(":(")}
+      />
+      <br />
+      <br />
+    </>
+  );
 }
